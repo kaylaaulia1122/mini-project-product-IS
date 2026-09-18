a2e764344a11960ca5e6d204c9301d8a8cae87cf
kaylaaulia1122 <kaylaaulia1122@gmail.com>
2026-09-18 17:21:29 +0700

    Create products.php

---
+++ products.php
@@ -0,0 +1,46 @@
+<?php
+
+$products = [
+    [
+        "id" => "PRD-001",
+        "nama" => "Laptop Asus Pro",
+        "kategori" => "Elektronik",
+        "harga" => 8500000,
+        "stok" => 5,
+        "deskripsi" => "Laptop kerja berperforma tinggi"
+    ],
+    [
+        "id" => "PRD-002",
+        "nama" => "Mouse Wireless",
+        "kategori" => "Aksesori",
+        "harga" => 150000,
+        "stok" => 2, 
+        "deskripsi" => "Mouse ergononis tanpa kabel"
+    ],
+    [
+        "id" => "PRD-003",
+        "nama" => "Keyboard Mekanikal",
+        "kategori" => "Aksesori",
+        "harga" => 450000,
+        "stok" => 1, 
+        "deskripsi" => "Keyboard mekanikal RGB switch biru"
+    ],
+    [
+        "id" => "PRD-004",
+        "nama" => "Monitor 24 Inch",
+        "kategori" => "Elektronik",
+        "harga" => 1800000,
+        "stok" => 8,
+        "deskripsi" => "Monitor IPS Full HD 75Hz"
+    ],
+    [
+        "id" => "PRD-005",
+        "nama" => "Flashdisk 64GB",
+        "kategori" => "Penyimpanan",
+        "harga" => 85000,
+        "stok" => 0, // Stok kritis (< 3)
+        "deskripsi" => "Flashdisk USB 3.0 kecepatan tinggi"
+    ]
+];
+
+  
